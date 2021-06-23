934fc965da6235518504a89b498b99b998b90f56
Metkel K <metkelk@gmail.com>
2021-06-23 18:43:18 -0400

    added loaded image test

--- components/Modal.test.tsx
+++ components/Modal.test.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import Modal from "./Modal";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 describe("Modal", () => {
   it("Renders Modal and displays data", () => {
     render(<Modal selectedImage={"/"} />);
   });
+
+  it("Checks image is loaded", () => {
+    render(<Modal selectedImage={"/alona-perez-p4xNF5ZPc0c-unsplash.jpg"} />);
+    const image = screen.getByRole("img");
+    expect(image).toHaveAttribute(
+      "src",
+      "/_next/image?url=%2Falona-perez-p4xNF5ZPc0c-unsplash.jpg&w=3840&q=100"
+    );
+  });
+
+  //   it("Checks modal closes with escape button", () => {
+
+  //   })
+
+  //   it("Checks modal closes by clicking outside of modal area", () => {
+
+  //   })
 });
