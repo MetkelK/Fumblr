f10b09a1c2270254892470c03d34c5fa42768434
Metkel K <metkelk@gmail.com>
2021-06-28 15:29:46 -0400

    added image component and close button

--- components/Modal.js
+++ components/Modal.js
@@ -1,12 +1,29 @@
+import Image from "next/image";
+
 const Modal = ({ selectedImage, setSelectedImage }) => {
   const handleClick = (e) => {
-    if (e.target.classList.contains("backdrop")) {
+    if (
+      e.target.classList.contains("backdrop") ||
+      e.target.classList.contains("close")
+    ) {
       setSelectedImage(null);
     }
   };
   return (
     <div className="backdrop" onClick={handleClick}>
-      <img src={selectedImage} alt="enlarged pic" />
+      <div className="backdrop-image">
+        <span className="close" onClick={() => handleClick}>
+          &times;
+        </span>
+        <Image
+          src={selectedImage}
+          alt="enlarged pic"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          priority
+        />
+      </div>
     </div>
   );
 };
